250cf48dc4db8b1daf60bc46629cfe65bc824d20
Veronicah Kanja <kanjaveronicah@gmail.com>
2025-09-17 12:22:29 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400) {
+    return "This one is on me!";
+  } else if (someValue > 2500) {
+    return "No can do.";
+  } else if (someValue > 2000) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "That will be twenty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      case "not as generous":
+        return "Thank you."
+      default:
+        return "Bye.";
+  }
 }
